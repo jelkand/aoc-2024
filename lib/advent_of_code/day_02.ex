940e51d65690b75fd92e2fd5d7060a2179c2b52c
Jack Anderson <jack@jackanderson.dev>
2024-12-02 07:07:44 -0800

    bad approach to d2p2

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -14,23 +14,47 @@ defmodule AdventOfCode.Day02 do
   def compute_line(line) do
     line
     |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.map(fn [a, b] -> a - b end)
-    |> test_line()
+    |> Enum.map(fn [a, b] -> b - a end)
   end
 
   def test_line(line) do
-    monotonic = Enum.all?(line, fn x -> x > 0 end) or Enum.all?(line, fn x -> x < 0 end)
+    monotonic_increasing = Enum.all?(line, fn x -> x > 0 end)
+    monotonic_decreasing = Enum.all?(line, fn x -> x < 0 end)
+
     within_bounds = Enum.all?(line, fn x -> abs(x) <= 3 end)
-    monotonic and within_bounds
+
+    {(monotonic_increasing or monotonic_decreasing) and within_bounds, count_errors(line)}
+  end
+
+  def count_errors(line) do
+    jump_too_large_errors = Enum.count(line, fn x -> abs(x) > 3 end)
+
+    # zeros are neither increasing nor decreasing so need to be handled separately
+    zero_errors = Enum.count(line, &(&1 == 0))
+
+    line = Enum.reject(line, &(&1 == 0))
+    {ascending, descending} = Enum.split_with(line, fn x -> x > 0 end)
+
+    y = jump_too_large_errors + zero_errors + min(length(ascending), length(descending))
+
+    dbg()
+    y
   end
 
   def part1(args) do
     args
     |> parse_input()
     |> Enum.map(&compute_line/1)
-    |> Enum.count(& &1)
+    |> Enum.map(&test_line/1)
+    |> Enum.count(&elem(&1, 0))
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> parse_input()
+    |> Enum.map(&compute_line/1)
+    |> Enum.map(&test_line/1)
+    |> Enum.count(fn line -> elem(line, 1) <= 1 end)
+    |> dbg()
   end
 end
--- test/advent_of_code/day_02_test.exs
+++ test/advent_of_code/day_02_test.exs
@@ -3,26 +3,23 @@ defmodule AdventOfCode.Day02Test do
 
   import AdventOfCode.Day02
 
+  @input """
+  7 6 4 2 1
+  1 2 7 8 9
+  9 7 6 2 1
+  1 3 2 4 5
+  8 6 4 4 1
+  1 3 6 7 9
+  """
   test "part1" do
-    input = """
-    7 6 4 2 1
-    1 2 7 8 9
-    9 7 6 2 1
-    1 3 2 4 5
-    8 6 4 4 1
-    1 3 6 7 9
-    """
-
-    result = part1(input)
+    result = part1(@input)
 
     assert result == 2
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@input)
 
-    assert result
+    assert result == 4
   end
 end
