292215fe92f8fd5eb75d2acf5bb45f46946acdcd
Jack Anderson <jack@jackanderson.dev>
2024-12-02 06:41:40 -0800

    d2p1

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,5 +1,34 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+  end
+
+  def parse_line(line) do
+    line
+    |> String.split(" ", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def compute_line(line) do
+    line
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [a, b] -> a - b end)
+    |> test_line()
+  end
+
+  def test_line(line) do
+    monotonic = Enum.all?(line, fn x -> x > 0 end) or Enum.all?(line, fn x -> x < 0 end)
+    within_bounds = Enum.all?(line, fn x -> abs(x) <= 3 end)
+    monotonic and within_bounds
+  end
+
+  def part1(args) do
+    args
+    |> parse_input()
+    |> Enum.map(&compute_line/1)
+    |> Enum.count(& &1)
   end
 
   def part2(_args) do
