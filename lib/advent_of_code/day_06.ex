9e42af656fd360ef271d762bce934c08e4d5c346
Jack Anderson <jack@jackanderson.dev>
2024-12-06 08:23:02 -0800

    parallelize

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -34,59 +34,19 @@ defmodule AdventOfCode.Day06 do
     end
   end
 
-  def solve_1({obstacles, start, size}) do
-    solve_1_internal(start, [], obstacles, size, :up)
+  def solve({obstacles, start, size}) do
+    solve_internal(start, [], obstacles, size, :up)
   end
 
-  def solve_1_internal({row, col}, visited, _obstacles, size, _dir)
+  def solve_internal({row, col}, visited, _obstacles, size, _dir)
       when row < 0 or row >= size or col < 0 or col >= size,
       do: visited
 
-  def solve_1_internal(pos, visited, obstacles, size, dir) do
+  def solve_internal(pos, visited, obstacles, size, dir) do
     {next_pos, next_dir} =
       get_next_pos_and_dir(pos, dir, obstacles)
 
-    solve_1_internal(next_pos, [pos | visited], obstacles, size, next_dir)
-  end
-
-  def solve_2({obstacles, start, size}) do
-    solve_2_internal(start, MapSet.new(), MapSet.new(), obstacles, size, :up)
-  end
-
-  def solve_2_internal({row, col}, _visited, potential_obstacles, _obstacles, size, _dir)
-      when row < 0 or row >= size or col < 0 or col >= size,
-      do: potential_obstacles
-
-  def solve_2_internal(pos, visited, potential_obstacles, obstacles, size, dir) do
-    potential_next_pos = get_next_pos(pos, dir)
-    potential_next_dir = get_next_dir(dir)
-
-    next_is_obstacle = MapSet.member?(obstacles, potential_next_pos)
-
-    obstacle_could_start_cycle =
-      !next_is_obstacle and
-        check_cycle(pos, visited, MapSet.put(obstacles, potential_next_pos), size, dir)
-
-    next_potential_obstacles =
-      case obstacle_could_start_cycle do
-        true -> MapSet.put(potential_obstacles, potential_next_pos)
-        false -> potential_obstacles
-      end
-
-    {next_pos, next_dir} =
-      case MapSet.member?(obstacles, potential_next_pos) do
-        true -> {get_next_pos(pos, potential_next_dir), potential_next_dir}
-        false -> {potential_next_pos, dir}
-      end
-
-    solve_2_internal(
-      next_pos,
-      MapSet.put(visited, {pos, dir}),
-      next_potential_obstacles,
-      obstacles,
-      size,
-      next_dir
-    )
+    solve_internal(next_pos, [pos | visited], obstacles, size, next_dir)
   end
 
   def check_cycle({row, col}, _visited, _obstacles, size, _dir)
@@ -133,7 +93,7 @@ defmodule AdventOfCode.Day06 do
 
   def part1(args) do
     parse_input(args)
-    |> solve_1()
+    |> solve()
     |> MapSet.new()
     |> MapSet.size()
   end
@@ -143,33 +103,16 @@ defmodule AdventOfCode.Day06 do
       inputs =
       parse_input(args)
 
-    # attempt1 = solve_2(inputs) |> IO.inspect()
-
-    attempt2 =
-      solve_1(inputs)
-      |> Enum.reverse()
-      # drop robot start
-      |> tl()
-      |> Enum.uniq()
-      |> Enum.filter(fn spot ->
-        check_cycle(robot_start, MapSet.new(), MapSet.put(obstacles, spot), max_size, :up)
+    solve(inputs)
+    |> Enum.reverse()
+    |> tl()
+    |> Enum.uniq()
+    |> Enum.map(fn obstacle ->
+      Task.async(fn ->
+        check_cycle(robot_start, MapSet.new(), MapSet.put(obstacles, obstacle), max_size, :up)
       end)
-      # |> dbg()
-      |> length()
-
-    # |> MapSet.new()
-
-    # |> IO.inspect(label: "new obstacles")
-    # |> length()
-
-    # |> MapSet.new()
-
-    # diff = MapSet.difference(attempt1, attempt2) |> dbg() |> MapSet.size()
-
-    # |> length()
-
-    # |> MapSet.size()
-    # 
-    # greater than 1650, less than 1747
+    end)
+    |> Task.await_many(:infinity)
+    |> length()
   end
 end
