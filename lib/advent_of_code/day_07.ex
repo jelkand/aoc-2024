561b8f2b97fdb094eeb8a9c4ac639fabb5d0878c
Jack Anderson <jack@jackanderson.dev>
2024-12-06 21:17:20 -0800

    d7

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -1,7 +1,39 @@
 defmodule AdventOfCode.Day07 do
-  def part1(_args) do
+  def parse_input(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      [test_value, inputs] = String.split(line, ":", trim: true)
+
+      inputs = inputs |> String.split(" ", trim: true) |> Enum.map(&String.to_integer/1)
+      {String.to_integer(test_value), inputs}
+    end)
   end
 
-  def part2(_args) do
+  def test_line(target, [], result), do: result == target
+
+  def test_line(target, [next | rest], result),
+    do: test_line(target, rest, result + next) or test_line(target, rest, result * next)
+
+  def test_line_2(target, _rest, result) when result > target, do: false
+  def test_line_2(target, [], result), do: result == target
+
+  def test_line_2(target, [next | rest], result),
+    do:
+      test_line_2(target, rest, result + next) or test_line_2(target, rest, result * next) or
+        test_line_2(target, rest, String.to_integer("#{result}#{next}"))
+
+  def part1(args) do
+    parse_input(args)
+    |> Enum.filter(fn {target, [first | inputs]} -> test_line(target, inputs, first) end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
+  end
+
+  def part2(args) do
+    parse_input(args)
+    |> Enum.filter(fn {target, [first | inputs]} -> test_line_2(target, inputs, first) end)
+    |> Enum.map(&elem(&1, 0))
+    |> Enum.sum()
   end
 end
