94fea4e8ef497b2b550504088d47a2b7f46fb8d1
Jack Anderson <jack@jackanderson.dev>
2024-12-09 21:56:54 -0800

    d10

--- lib/advent_of_code/day_10.ex
+++ lib/advent_of_code/day_10.ex
@@ -1,7 +1,52 @@
 defmodule AdventOfCode.Day10 do
-  def part1(_args) do
+  alias AdventOfCode.Helpers
+
+  def parse_input(input) do
+    matrix =
+      String.split(input, "\n", trim: true)
+      |> Enum.map(
+        &(String.split(&1, "", trim: true)
+          |> Enum.map(fn str -> String.to_integer(str) end))
+      )
+
+    {Helpers.matrix_to_point_map(matrix), length(matrix)}
   end
 
-  def part2(_args) do
+  def find_score_trailheads({point_map, size}) do
+    potential_trailheads = Enum.filter(point_map, fn {_k, v} -> v == 0 end)
+
+    Enum.map(potential_trailheads, &score_trailhead(&1, point_map, size))
+  end
+
+  def score_trailhead({pt, 9}, _, _), do: [{pt, 9}]
+
+  def score_trailhead({{row, col}, elev}, map, size) do
+    for r_diff <- -1..1,
+        c_diff <- -1..1,
+        abs(r_diff) != abs(c_diff) do
+      {row + r_diff, col + c_diff}
+    end
+    |> Enum.filter(fn {r, c} -> r >= 0 and c >= 0 and r < size and c < size end)
+    |> Enum.filter(fn pos -> Map.get(map, pos) - elev == 1 end)
+    |> Enum.map(fn pos -> {pos, Map.get(map, pos)} end)
+    |> case do
+      [] -> []
+      neighbors -> Enum.flat_map(neighbors, fn pt -> score_trailhead(pt, map, size) end)
+    end
+  end
+
+  def part1(args) do
+    parse_input(args)
+    |> find_score_trailheads()
+    |> Enum.map(&MapSet.new/1)
+    |> Enum.map(&MapSet.size/1)
+    |> Enum.sum()
+  end
+
+  def part2(args) do
+    parse_input(args)
+    |> find_score_trailheads()
+    |> List.flatten()
+    |> length()
   end
 end
--- test/advent_of_code/day_10_test.exs
+++ test/advent_of_code/day_10_test.exs
@@ -3,19 +3,25 @@ defmodule AdventOfCode.Day10Test do
 
   import AdventOfCode.Day10
 
-  @tag :skip
+  @input """
+  89010123
+  78121874
+  87430965
+  96549874
+  45678903
+  32019012
+  01329801
+  10456732
+  """
   test "part1" do
-    input = nil
-    result = part1(input)
+    result = part1(@input)
 
-    assert result
+    assert result == 36
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@input)
 
-    assert result
+    assert result == 81
   end
 end
