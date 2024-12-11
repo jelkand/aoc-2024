45aeee02cca89203b5922e2a50c8cc96b84fb00b
Jack Anderson <jack@jackanderson.dev>
2024-12-10 21:19:59 -0800

    d11p1

--- lib/advent_of_code/day_11.ex
+++ lib/advent_of_code/day_11.ex
@@ -1,7 +1,47 @@
 defmodule AdventOfCode.Day11 do
-  def part1(_args) do
+  def parse_input(input) do
+    String.trim(input)
+    |> String.split(" ", trim: true)
+    |> Enum.map(&String.to_integer/1)
   end
 
-  def part2(_args) do
+  def blink(list) do
+    blink_internal(list, [])
+  end
+
+  def blink_internal([], acc), do: Enum.reverse(acc)
+  def blink_internal([0 | rest], acc), do: blink_internal(rest, [1 | acc])
+
+  def blink_internal([first | rest], acc) do
+    digits = Integer.digits(first)
+
+    length = length(digits)
+
+    # dbg(binding(), charlists: :as_lists)
+
+    case Integer.mod(length, 2) do
+      0 ->
+        {l, r} = Enum.split(digits, div(length, 2))
+        blink_internal(rest, [Integer.undigits(r) | [Integer.undigits(l) | acc]])
+
+      _ ->
+        blink_internal(rest, [first * 2024 | acc])
+    end
+  end
+
+  def blink_times(list, times) do
+    Enum.reduce(1..times, list, fn _, acc -> blink(acc) end)
+  end
+
+  def part1(args) do
+    parse_input(args)
+    |> blink_times(25)
+    |> length()
+  end
+
+  def part2(args) do
+    parse_input(args)
+    |> blink_times(50)
+    |> length()
   end
 end
--- test/advent_of_code/day_11_test.exs
+++ test/advent_of_code/day_11_test.exs
@@ -3,17 +3,15 @@ defmodule AdventOfCode.Day11Test do
 
   import AdventOfCode.Day11
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "125 17"
     result = part1(input)
 
-    assert result
+    assert result == 55312
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "0"
     result = part2(input)
 
     assert result
