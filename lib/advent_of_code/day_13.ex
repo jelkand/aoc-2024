d4d2464811961969781e08d6b060dbf870f68c21
Jack Anderson <jack@jackanderson.dev>
2024-12-12 21:30:00 -0800

    d13.p1

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -1,5 +1,58 @@
 defmodule AdventOfCode.Day13 do
-  def part1(_args) do
+  def parse_input(input) do
+    String.split(input, "\n\n", trim: true)
+    |> Enum.map(&parse_puzzle/1)
+  end
+
+  def parse_puzzle(puzzle) do
+    String.split(puzzle, "\n", trim: true)
+    |> Enum.map(
+      &(Regex.run(~r/X[+=](\d+).*Y[+=](\d+)/, &1, capture: :all_but_first)
+        |> Enum.map(fn n -> String.to_integer(n) end)
+        |> List.to_tuple())
+    )
+    |> List.to_tuple()
+  end
+
+  # def solve_puzzles(puzzles)
+  #   Enum.map()
+  # end
+  # {{a_x, a_y}, {b_x, b_y}, {t_x, t_y}}
+  # 
+
+  def solve_puzzle({{a_x, a_y}, _, {t_x, t_y}}, count, solutions)
+      when a_x * count > t_x or a_y * count > t_y,
+      do: solutions
+
+  def solve_puzzle({{a_x, a_y}, {b_x, b_y}, {t_x, t_y}} = puzzle, count, solutions) do
+    mult_a_x = a_x * count
+    mult_a_y = a_y * count
+
+    remainder_x = t_x - mult_a_x
+    remainder_y = t_y - mult_a_y
+
+    cond do
+      Integer.mod(remainder_x, b_x) == 0 and Integer.mod(remainder_y, b_y) == 0 and
+          div(remainder_x, b_x) == div(remainder_y, b_y) ->
+        solve_puzzle(puzzle, count + 1, [{count, div(remainder_x, b_x)} | solutions])
+
+      true ->
+        solve_puzzle(puzzle, count + 1, solutions)
+    end
+  end
+
+  def score_solutions([]), do: 0
+
+  def score_solutions(list) do
+    Enum.map(list, fn {a, b} -> 3 * a + b end)
+    |> Enum.min()
+  end
+
+  def part1(args) do
+    parse_input(args)
+    |> Enum.map(&solve_puzzle(&1, 1, []))
+    |> Enum.map(&score_solutions/1)
+    |> Enum.sum()
   end
 
   def part2(_args) do
--- test/advent_of_code/day_13_test.exs
+++ test/advent_of_code/day_13_test.exs
@@ -3,18 +3,31 @@ defmodule AdventOfCode.Day13Test do
 
   import AdventOfCode.Day13
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input """
+  Button A: X+94, Y+34
+  Button B: X+22, Y+67
+  Prize: X=8400, Y=5400
 
-    assert result
+  Button A: X+26, Y+66
+  Button B: X+67, Y+21
+  Prize: X=12748, Y=12176
+
+  Button A: X+17, Y+86
+  Button B: X+84, Y+37
+  Prize: X=7870, Y=6450
+
+  Button A: X+69, Y+23
+  Button B: X+27, Y+71
+  Prize: X=18641, Y=10279
+  """
+  test "part1" do
+    result = part1(@input)
+
+    assert result == 480
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
+    result = part2(@input)
 
     assert result
   end
